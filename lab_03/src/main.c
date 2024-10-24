ec444e4eb4cd22685d675694d945059a656c747a
Nikolai2038 <nikolai2038@yandex.ru>
2024-10-24 12:25:23 +0300

    Revert "Lab 03: Add new code"
    This reverts commit 66744572654a42f604d994c863d6ab7a49158641.

--- lab_03/src/main.c
+++ lab_03/src/main.c
@@ -5,87 +5,44 @@
 #define UARTBASE 0xE0000000
 
 struct uart_info {
-    void *iobase;
+	void *iobase;
 };
 
 static struct uart_info uart0_data;
 static struct uart_info *uart0 = &uart0_data;
 
-// Функция для передачи символа через UART
 static void UART_Tx(struct uart_info *uart, char c) {
-    void *iobase = uart->iobase;
-    u32 sr;
-    do {
-        sr = readl(iobase + CDNS_UART_SR);
-    } while (sr & CDNS_UART_SR_TXFULL); // Ожидаем, пока буфер не освободится
-    writel(c, iobase + CDNS_UART_FIFO); // Отправляем символ
+	void *iobase;
+	u32 sr;
+	iobase = uart->iobase;
+	do {
+		sr = readl(iobase + CDNS_UART_SR);
+	} while (sr & CDNS_UART_SR_TXFULL);
+	writel(c, iobase +  CDNS_UART_FIFO);
 }
 
-// Функция для приема символа через UART
-static char UART_Rx(struct uart_info *uart) {
-    void *iobase = uart->iobase;
-    u32 sr;
-    char c;
-    do {
-        sr = readl(iobase + CDNS_UART_SR);
-    } while (!(sr & CDNS_UART_SR_RXEMPTY)); // Ждем, пока данные будут готовы
-    c = readl(iobase + CDNS_UART_FIFO);     // Читаем полученный символ
-    return c;
-}
-
-// Функция инициализации UART
 static void UART_Init(struct uart_info *uart, void *init_iobase) {
-    void *iobase = uart->iobase = init_iobase;
-    u32 cr = readl(iobase + CDNS_UART_CR);
-    writel(cr |= CDNS_UART_CR_TX_DIS, iobase + CDNS_UART_CR); // Отключаем передачу
-    writel(cr |= CDNS_UART_CR_TXRST, iobase + CDNS_UART_CR);  // Сбрасываем передатчик
-    do {
-        cr = readl(iobase + CDNS_UART_CR);
-    } while (cr & CDNS_UART_CR_TXRST); // Ждем завершения сброса
-    writel(cr = (cr & ~CDNS_UART_CR_TX_DIS) | CDNS_UART_CR_TX_EN, iobase + CDNS_UART_CR); // Включаем передачу
+	void *iobase;
+	u32 cr;
+	iobase = uart->iobase = init_iobase;
+	cr = readl(iobase + CDNS_UART_CR);
+	writel(cr |= CDNS_UART_CR_TX_DIS, iobase +  CDNS_UART_CR);
+	writel(cr |= CDNS_UART_CR_TXRST, iobase +  CDNS_UART_CR);
+	do
+		cr = readl(iobase + CDNS_UART_CR);
+	while (cr & CDNS_UART_CR_TXRST);
+	writel(cr = (cr & ~CDNS_UART_CR_TX_DIS) | CDNS_UART_CR_TX_EN, iobase +  CDNS_UART_CR);
 }
 
-// Вывод строки через UART
 static void PrintSZ(char *p) {
-    char c;
-    while ((c = *(p++))) {
-        UART_Tx(uart0, c);
-    }
+	char c;
+	while ((c = *(p++))) {
+		UART_Tx(uart0, c);
+	}
 }
 
-// Чтение числа из UART
-static int ReadNumber() {
-    char buffer[10];
-    int i = 0;
-    char c;
-    while (1) {
-        c = UART_Rx(uart0); // Ждем символа от пользователя
-        if (c == '\r') {    // Конец ввода по символу возврата каретки
-            buffer[i] = '\0';
-            break;
-        } else {
-            buffer[i++] = c;
-            UART_Tx(uart0, c); // Эхо-ввод
-        }
-    }
-    return atoi(buffer); // Преобразование строки в число
-}
-
-// Главная функция программы
 void start(void) {
-    UART_Init(uart0, (void *)UARTBASE);
-
-    while (1) {
-        PrintSZ("\r\nEnter first number: ");
-        int num1 = ReadNumber(); // Чтение первого числа
-
-        PrintSZ("\r\nEnter second number: ");
-        int num2 = ReadNumber(); // Чтение второго числа
-
-        int sum = num1 + num2; // Сложение чисел
-
-        char result[32];
-        snprintf(result, sizeof(result), "\r\nSum: %d\r\n", sum);
-        PrintSZ(result); // Вывод результата
-    }
+	UART_Init(uart0, (void *)UARTBASE);
+	PrintSZ("\r\n!!! Sample code is running !!!\r\n");
+	return;
 }
